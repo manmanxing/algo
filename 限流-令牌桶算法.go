73e56995f9c96beb51e8abc6c81555d7b2d6ad80
zengjie <zengjie@qingsongchou.com>
2021-02-13 14:06:24 +0800

    add 限流算法-令牌桶

---
+++ 限流-令牌桶算法.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"sync"
+	"time"
+)
+
+type TokenBucket struct {
+	Rate         int64 //固定的token放入速率(每秒)
+	Capacity     int64 //桶的容量
+	Tokens       int64 //桶中当前的token数量
+	LastTokenSec int64 //桶上次放 token 的时间戳(秒)
+	Lock         sync.Mutex
+}
+
+//先计算放入的token数量,需要判断token是否溢出
+//再计算剩余的token数量，如果 >= 0 返回 true，否则返回 false
+func (t *TokenBucket) Allow()bool {
+	now := time.Now().Unix()
+	addTokens := (now - t.LastTokenSec) * t.Rate
+	t.LastTokenSec = now
+	t.Tokens = t.Tokens + addTokens
+	if t.Tokens > t.Capacity {
+		t.Tokens = t.Capacity
+	}
+	if t.Tokens  > 0 {
+		t.Tokens --
+		return true
+	}else {
+		//不足以获取这么多的 token
+		return false
+	}
+}
+
+func (t *TokenBucket) Set(rate, capacity int64) {
+	if rate < 0 || capacity < 0 {
+		return
+	}
+	t.Rate = rate
+	t.Capacity = capacity
+	t.Tokens = 0
+	t.LastTokenSec = time.Now().Unix()
+}
--- 限流-漏桶算法.go
+++ 限流-漏桶算法.go
@@ -9,30 +9,26 @@ type LeakyBucket struct {
 	Rate       float64 //固定的出水速率(每秒)
 	Capacity   float64 //桶的容量
 	Water      float64 //桶中当前的水量
-	lastLeakMs int64   //桶上次漏水时间戳(毫秒)
+	LastLeakMs int64   //桶上次漏水时间戳(毫秒)
 	Lock       sync.Mutex
 }
 
-//n表示进水量
-//先计算漏水量
+//先计算漏水量,需判断桶是否干了
 //如果桶中当前的水量+n 没有超过桶的容量，返回 true，否则返回 false
-func (l *LeakyBucket) Allow(n float64) bool {
-	if n < 0 {
-		return false
-	}
+func (l *LeakyBucket) Allow() bool {
 	now := time.Now().UnixNano() / 1e6
-	eclipseWater := float64(now-l.lastLeakMs) * l.Rate / 1000
+	eclipseWater := float64(now-l.LastLeakMs) * l.Rate / 1000
 	l.Water = l.Water - eclipseWater
 	//如果桶干了
 	if l.Water < 0 {
 		l.Water = 0
 	}
-	l.lastLeakMs = now
+	l.LastLeakMs = now
 	//判断水量
-	if l.Water + n <= l.Capacity {
-		l.Water = l.Water + n
+	if l.Water + 1 <= l.Capacity {
+		l.Water ++
 		return true
-	}else {
+	} else {
 		return false
 	}
 }
@@ -45,5 +41,5 @@ func (l *LeakyBucket) Set(rate, Capacity float64) {
 	l.Rate = rate
 	l.Capacity = Capacity
 	l.Water = 0
-	l.lastLeakMs = time.Now().UnixNano() / 1e6
+	l.LastLeakMs = time.Now().UnixNano() / 1e6
 }
